82b926c28d233cb5a7b99b5c8447d79f36fd8642
Killian DROULEZ <killian.droulez@rwigo.com>
2024-06-07 17:15:00 +0200

    rust: solve luhn exercice

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,27 +1,32 @@
 /// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
-    // TODO: Find better explanation of luhn algo (difference between sources)
-    
+pub fn is_valid(code: &str) -> bool {    
     if code.len() <= 1 { return false; }
+
+    // Removing whitespaces
     let stripped = code.trim();
     let whitespace_free: String = stripped.chars().filter(|c| !c.is_whitespace()).collect();
-    let only_digit = whitespace_free.as_bytes().iter().all(|d| (0x30..0x40).contains(d));
-    println!("original: {}", code);
-    if !only_digit { println!("NOT only digits"); return false; }
-    println!("only digits");
-    println!("whitespace_free: {}", whitespace_free);
-    let doubled = whitespace_free.chars().enumerate().map(|(i, d)| {
+
+    // Ensuring all chars are only numeric
+    let only_digit = whitespace_free.chars().all(|c| c.is_numeric());
+    if !only_digit { println!("Must only contains digits"); return false; }
+    let not_unicode = whitespace_free.as_bytes().iter().all(|d| (0x30..0x40).contains(d));
+    if !not_unicode { println!("Must not contain unicode"); return false; }
+
+    // Calculating doubled digits sum
+    let mut digits_iter_num = 0;
+    let doubled = whitespace_free.chars().rev().enumerate().map(|(i, d)| {
+        digits_iter_num += 1;
         let digit = d.to_digit(10).unwrap_or(0);
         if i % 2 != 0 {
-            println!("double {}", digit);
             let mut doubled = digit * 2;
             if doubled > 9 { doubled -= 9; }
             return doubled
         } else { return digit; }
     }).collect::<Vec<u32>>();
-    let sum = doubled.iter().fold(0, |acc, v| acc + v);
-    
-    println!("sum: {}", sum);
 
-    sum != 0 && sum % 10 != 0
+    // Handling corner case single_zero_with_space_is_invalid
+    if digits_iter_num == 1 && code.contains("0") && code.contains(" ") { return false; }
+
+    let sum = doubled.iter().fold(0, |acc, v| acc + v);
+    sum % 10 == 0
 }
